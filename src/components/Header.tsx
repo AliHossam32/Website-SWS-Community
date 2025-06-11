61402f9eecbb74e52e3f2b3bef3d853b76dc62ce
Ali Hossam <kingalihossam11@gmail.com>
2025-06-11 15:24:05 +0300

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
           {/* Logo */}
           <div className="flex items-center justify-center">
             <img 
-              src="../logo/SWS logo.png" 
+              src="../SWS logo.png" 
               alt="Logo" 
               className="h-10 md:h-11 w-auto object-contain transition-all duration-200" 
             />
